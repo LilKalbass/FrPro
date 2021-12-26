1658c84ac18e8f15ade50ac0f67bd66728b4fc84
Artem Dimoglov <adimoglov@gmail.com>
2021-12-26 19:57:23 +0200

    first commitgit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.time("s");
+console.dir(document.location + "'hello,wrld'");
+console.group();
+console.info("Quite important");
+console.count("Greetings");console.count("Greetings");console.count("Greetings");console.count("Greetings");
+console.log("hi, world");
+console.groupEnd();
+console.warn("bye, world");
+console.timeEnd("s");
