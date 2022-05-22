cf63fc11ff4a54e32b57346afe93c8c80be5ab55
Artem Dimoglov <adimoglov@gmail.com>
2022-05-23 01:25:39 +0300

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+let getAdder = function ()
+{
+    var res = 0;
+    return function (x)
+    {
+        res += x;
+        return res;
+    }
+};
+let sum = getAdder();
+sum(3) //-> ф-ция возвращает результат 3
+sum(5) //-> ф-ция возвращает результат 8
+sum(20) //-> ф-ция возвращает результат 28
+debugger;
