1d8d5979ce51e8cb7f3778f3941a9c739d25fd04
Artem Dimoglov <adimoglov@gmail.com>
2022-05-23 00:51:46 +0300

    doneCopy

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log("hello world");
+
+function isObject(object) {
+    return typeof object === "object" && object !== null && !Array.isArray(object);
+}
+
+// Написать функцию “глубокого” копирования. Функция принимает один параметр и 
+// возвращает его копию “по значению”. В функцию можно передать параметр любого типа. 
+// Если передали объект, то предусмотреть ситуацию, 
+// когда свойствами этого объекта будут объекты или массивы.
+// Если передали массив, то предусмотреть ситуацию, 
+// когда элементами этого массива будут объекты или массивы.
+// Если передали примитив - вернуть его. 
+
+function deepCopy(input) 
+{
+    if (isObject(input) === true/*||Array.isArray(input) === true*/) {
+        let value = {};
+        for (let key in input) {
+            if (isObject(input[ key ])) {
+                value[ key ] = deepCopy(input[ key ])
+            }
+            else {
+                value[ key ] = input[ key ]
+            };
+        };
+        return value
+    }
+    else if (Array.isArray(input) === true) {
+        let value = [];
+        for (let key in input) {
+            if (Array.isArray(input[ key ])) {
+                value[ key ] = deepCopy(input[ key ])
+            } else {
+                value[ key ] = input[ key ]
+            }
+        } return value;
+    }
+    else {
+        return input;
+    };
+}
+
+const a = {
+    name:"qwertyyyyyyyy",
+    surname:"ytrewqqqqqqqq",
+}
+let b = deepCopy(a)
+console.log(b);
+
+arr = [ "hey budddy", 1, 2, 3, 4, 5 ];
+
+let q = deepCopy(arr);
+console.log(q);
